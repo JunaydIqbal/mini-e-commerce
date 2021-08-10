e499099ba68038de132310f0348fbc152a660d12
thejunaidiqbal1 <thejunaidiqbal@outlook.com>
2021-08-10 12:42:06 +0500

    problems solve

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,8 @@ module MiniECommerce
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.assets.initialize_on_precompile = false
-    config.assets.debug = true
+    #config.assets.initialize_on_precompile = false
+    #config.assets.debug = true
 
     # Configuration for the application, engines, and railties goes here.
     #
