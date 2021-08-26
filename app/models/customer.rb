c0a5ff0877b62da7578c2c701db3849ba765979b
thejunaidiqbal1 <thejunaidiqbal@outlook.com>
2021-08-26 20:35:49 +0500

    bugs fix

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -12,27 +12,27 @@ class Customer < ApplicationRecord
 
   def self.from_omniauth(access_token)
     data = access_token.info
-    customer = Customer.where(email: data['email']).first
+    #customer = Customer.where(email: data['email']).first
     
     # # Uncomment the section below if you want users to be created if they don't exist
-    unless customer
-      customer = Customer.create(username: data['name'].length > 15 ? data['name'].slice(0..14).gsub(/\s+/, "") : data['name'].gsub(/\s+/, ""),
-           email: data['email'],
-           phone: data['phone'],
-           name: data['name'],
-           password: Devise.friendly_token[0,20]
-        )
-    end
+    # unless customer
+    #   customer = Customer.create(username: data['name'].length > 15 ? data['name'].slice(0..14).gsub(/\s+/, "") : data['name'].gsub(/\s+/, ""),
+    #        email: data['email'],
+    #        phone: data['phone'],
+    #        name: data['name'],
+    #        password: Devise.friendly_token[0,20]
+    #     )
+    # end
     # customer.cid = data['uid']
     # customer.provider = data['provider']
-    customer.save
-    # where(provider: data.provider, cid: data.uid).first_or_create do |cust|
-    #   cust.email = data.email
-    #   cust.username = data.name.length > 15 ? data.name.slice(0..14).gsub(/\s+/, "") : data.name.gsub(/\s+/, "")
-    #   cust.name = data.name
-    #   cust.password = Devise.friendly_token[0,20]
-    # end
-    customer
+    # customer.save
+    where(provider: data.provider, cid: data.uid).first_or_initialize do |cust|
+      cust.email = data.email
+      cust.username = data.name.length > 15 ? data.name.slice(0..14).gsub(/\s+/, "") : data.name.gsub(/\s+/, "")
+      cust.name = data.name
+      cust.password = Devise.friendly_token[0,20]
+    end
+    
   end
 
   def to_s
