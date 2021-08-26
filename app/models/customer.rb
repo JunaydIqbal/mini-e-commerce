7427bb71396c97457f90c72b02acb888a3b7637e
thejunaidiqbal1 <thejunaidiqbal@outlook.com>
2021-08-26 20:38:37 +0500

    bugs fix

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -26,13 +26,15 @@ class Customer < ApplicationRecord
     # customer.cid = data['uid']
     # customer.provider = data['provider']
     # customer.save
-    where(provider: data.provider, cid: data.uid).first_or_initialize do |cust|
+    customer = where(provider: data.provider, cid: data.uid).first_or_initialize do |cust|
       cust.email = data.email
       cust.username = data.name.length > 15 ? data.name.slice(0..14).gsub(/\s+/, "") : data.name.gsub(/\s+/, "")
       cust.name = data.name
       cust.password = Devise.friendly_token[0,20]
     end
-    
+
+    customer.save
+    customer
   end
 
   def to_s
